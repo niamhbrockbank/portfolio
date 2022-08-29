fe0f045c5957db62796fcecb9bb0f202b810d5b9
Niamh <niamh.brockbank@gmail.com>
2022-08-29 16:13:46 +0100

    Return value of goToGitHubRepo

--- src/utils/goToGitHubRepo.ts
+++ src/utils/goToGitHubRepo.ts
@@ -1,5 +1,5 @@
 import { Project } from "../types";
 
-export default function goToGitHubRepo(project: Project) {
+export default function goToGitHubRepo(project: Project): void {
   window.open(project.github_link, "_blank");
 }
