313e296cd2dc3a6f2f855a3be89a231693230ab0
Niamh <niamh.brockbank@gmail.com>
2022-08-29 13:06:44 +0100

    Format with prettier

--- src/components/FeaturedProjects.tsx
+++ src/components/FeaturedProjects.tsx
@@ -1,13 +1,13 @@
-export default function FeaturedProjects():JSX.Element {
-    return (
-        <>
-        <h1>featured</h1>
+export default function FeaturedProjects(): JSX.Element {
+  return (
+    <>
+      <h1>featured</h1>
       <ul id="featured_list">
         <li className="featured_project">project one</li>
         <li className="featured_project">project two</li>
         <li className="featured_project">project three</li>
         <li className="featured_project">project 4</li>
       </ul>
-      </>
-    )
+    </>
+  );
 }
