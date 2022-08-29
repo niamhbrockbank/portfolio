311d512e90c7bf1d3b632b3c598bfb65768e15b1
Niamh <niamh.brockbank@gmail.com>
2022-08-29 11:03:43 +0100

    Format CSS menu bar

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,13 +1,14 @@
 export default function MenuBar():JSX.Element {
     return (
         <>
-            <ul id='menu_list'>
-                <li>Niamh Brockbank</li>
-                <li>Featured</li>
-                <li>Archive</li>
-                <li>Info</li>
+            <ul id='menu_bar'>
+                <li id='menu_name'>Niamh Brockbank</li>
+                <div id='menu_list'>
+                    <li className="menu_list_option">FEATURED</li>
+                    <li className="menu_list_option">ARCHIVE</li>
+                    <li className="menu_list_option">INFO</li>
+                </div>
             </ul>
-            <hr />
         </>
     )
 }
