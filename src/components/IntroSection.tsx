0e8b4fa4bdd620ee16e7aa7498e3b042beb789d9
Niamh <niamh.brockbank@gmail.com>
2022-08-29 12:06:38 +0100

    Format code with prettier

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -1,9 +1,12 @@
 export default function IntroSection(): JSX.Element {
   return (
     <>
-      <div id='intro_section'>
+      <div id="intro_section">
         <h1>Intro</h1>
-        <p id='intro'>Junior Software Developer pursuing creative and beautiful solutions to big problems.</p>
+        <p id="intro">
+          Junior Software Developer pursuing creative and beautiful solutions to
+          big problems.
+        </p>
       </div>
     </>
   );
