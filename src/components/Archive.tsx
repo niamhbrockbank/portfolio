0f913079e846e789f148056bd47398d191c86d21
Niamh <niamh.brockbank@gmail.com>
2022-08-29 15:25:48 +0100

    Add basic list of projects to archive

--- src/components/Archive.tsx
+++ src/components/Archive.tsx
@@ -1,13 +1,29 @@
+import { Project } from "../types";
+
 export default function Archive(): JSX.Element {
+  const archiveProjects : Project[] = [
+    {
+      name : 'to-do app',
+      description : 'full-stack app that allows a user to insert and delete tasks.',
+      image : 'no image yet'
+    },
+    {
+      name : 'tv show picker',
+      description : 'front-end app that fetches tv show data from an API and can navigate between shows with a dropdown',
+      image : 'no image yet'
+    },
+    {
+      name : 'baby names picker',
+      description : 'front-end app that can be used to choose baby names',
+      image : 'no image yet'
+    }
+  ]
+
   return (
     <>
       <h1 id='archive_title'>Archive</h1>
       <ul id="archive_list">
-        <li className="archive_project">archive 1</li>
-        <li className="archive_project">archive 2</li>
-        <li className="archive_project">archive 3</li>
-        <li className="archive_project">archive 4</li>
-        <li className="archive_project">archive 5</li>
+        {archiveProjects.map((project) => <li className="archive_project" key={archiveProjects.indexOf(project)}>{project.name.toLowerCase()}</li>)}
       </ul>
     </>
   );
