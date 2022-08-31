0f8be9b278e110cf203341e8aefe9188d91926a6
Niamh <niamh.brockbank@gmail.com>
2022-08-31 19:06:40 +0100

    Add links to repos and deployed apps

--- src/components/ProjectPage.tsx
+++ src/components/ProjectPage.tsx
@@ -16,17 +16,17 @@ export default function ProjectPage({
     return <h1>Project cannot be found.</h1>;
   }
 
-  const { name, description, deployed_link, github_link } = currentProject;
+  const { name, description, deployed_link} = currentProject;
 
   return (
     <div id="project_page">
       <h1>{name}</h1>
+      <p>{description}</p>
       <ul>
-        <li>{description}</li>
-        <li>deployed app: {deployed_link}</li>
-        {github_link && <li>github repo: {github_link}</li>}
-        <li>date</li>
-        <li>languages used</li>
+        <li><a href={deployed_link}>find the project output here</a></li>
+        {currentProject.github_link && <li><a href={currentProject.github_link}>find the GitHub repo here</a></li>}
+        <li>date created:</li>
+        <li>languages used: </li>
         <li>image</li>
       </ul>
     </div>
