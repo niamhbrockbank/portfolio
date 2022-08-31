6245c16b63be86c5ed81ae6764017677bbc84890
Niamh <niamh.brockbank@gmail.com>
2022-08-31 16:44:39 +0100

    Add feature section back in

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,11 +1,11 @@
 import Archive from "./Archive";
-// import FeaturedProjects from "./FeaturedProjects";
+import FeaturedProjects from "./FeaturedProjects";
 import projectsArchive from "./projectsArchive.json";
 
 export default function ProjectsSection(): JSX.Element {
   return (
     <>
-      {/* <FeaturedProjects projectsArchive={projectsArchive} /> */}
+      <FeaturedProjects projectsArchive={projectsArchive} />
       <Archive projectsArchive={projectsArchive} />
     </>
   );
