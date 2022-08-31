d16768aeee5bb31f0a94886cd13c674a5f3414f9
Niamh <niamh.brockbank@gmail.com>
2022-08-31 12:03:14 +0100

    Connect featured section with projects archive

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,11 +1,12 @@
 import Archive from "./Archive";
 import FeaturedProjects from "./FeaturedProjects";
+import projectsArchive from "./archiveProjects.json";
 
 export default function ProjectsSection(): JSX.Element {
   return (
     <>
-      <FeaturedProjects />
-      <Archive />
+      <FeaturedProjects projectsArchive= {projectsArchive}/>
+      <Archive projectsArchive= {projectsArchive}/>
     </>
   );
 }
---
+++ src/utils/createPlaceholderFeats.tsx
@@ -0,0 +1,8 @@
+
+export default function createPlaceholderFeats(numPlaceholders : number): JSX.Element[]{
+    return [...Array(numPlaceholders)].map((i) => createComingSoonElement(i))
+}
+
+function createComingSoonElement(key : number): JSX.Element{
+    return <li className="featured_project" key={key}>coming soon</li>
+  }
