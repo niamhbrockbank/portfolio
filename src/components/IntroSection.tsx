8755125efccba14a0c4b28a7931e87d312371c1c
niamhbrockbank <niamh.brockbank@gmail.com>
2022-09-27 13:48:57 +0100

    Change intro to entry level engineer

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -4,7 +4,7 @@ export default function IntroSection(): JSX.Element {
       <div id="intro_section">
         <h1>Intro</h1>
         <p id="intro">
-          Junior Software Developer pursuing creative and beautiful solutions to
+          Entry Level Software Developer pursuing creative and beautiful solutions to
           big problems.
         </p>
       </div>
