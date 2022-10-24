0097761e4f2b1b75539c97928022a4a829e6d18e
niamhbrockbank <niamh.brockbank@gmail.com>
2022-10-24 14:05:53 +0100

    Format with prettier

--- src/components/Home/IntroSection.tsx
+++ src/components/Home/IntroSection.tsx
@@ -4,7 +4,8 @@ export default function IntroSection(): JSX.Element {
       <div id="intro_section">
         <h1>Intro</h1>
         <p id="intro">
-          My name is Niamh Brockbank, I'm a <i>software developer</i> living and working in the UK.
+          My name is Niamh Brockbank, I'm a <i>software developer</i> living and
+          working in the UK.
         </p>
       </div>
     </>
