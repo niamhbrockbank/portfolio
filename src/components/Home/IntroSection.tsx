505472fed32b0a425d3411eacdd9f543d3b9522e
niamhbrockbank <niamh.brockbank@gmail.com>
2022-10-26 18:40:44 +0100

    Style intro with picture

--- src/components/Home/IntroSection.tsx
+++ src/components/Home/IntroSection.tsx
@@ -8,7 +8,7 @@ export default function IntroSection(): JSX.Element {
           My name is Niamh Brockbank, I'm a <i>software developer</i> living and
           working in the UK.
           </p>
-          <img id="profile_photo" src="../img/IMG_7976.jpeg"></img>
+          <img id="profile_photo" src="../img/IMG_7976.jpeg" alt="Niamh Brockbank generated using Stable Diffusion"></img>
         </div>
       </div>
     </>
