ab20ce38f6d0eae5a1e0fd26046ca7ec95342c22
niamhbrockbank <niamh.brockbank@gmail.com>
2022-10-27 11:00:25 +0100

    Add backend to resources sharer

--- src/components/Home/IntroSection.tsx
+++ src/components/Home/IntroSection.tsx
@@ -4,11 +4,15 @@ export default function IntroSection(): JSX.Element {
       <div id="intro_section">
         <h1>Intro</h1>
         <div id="intro">
-          <p id='intro_words'>
-          My name is Niamh Brockbank, I'm a <i>software developer</i> living and
-          working in the UK.
+          <p id="intro_words">
+            My name is Niamh Brockbank, I'm a <i>software developer</i> living
+            and working in the UK.
           </p>
-          <img id="profile_photo" src="../img/IMG_7976.jpeg" alt="Niamh Brockbank generated using Stable Diffusion"></img>
+          <img
+            id="profile_photo"
+            src="../img/IMG_7976.jpeg"
+            alt="Niamh Brockbank generated using Stable Diffusion"
+          ></img>
         </div>
       </div>
     </>
