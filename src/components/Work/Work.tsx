70e6dc8dfe392f90364e56d175ffbc377fb3493d
niamhbrockbank <niamh.brockbank@gmail.com>
2022-11-13 18:57:39 +0000

    Move ProjectSection and Archive to Work func comp

--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -1,11 +1,73 @@
+// import { Link } from "react-router-dom";
 import { Project } from "../../types";
-import ProjectsSection from "./ProjectsSection";
 import './Work.scss'
 
+//TODO: change all function component props to IProps
 interface IProps {
-    projectsArchive: Project[];
-  }
+  projectsArchive: Project[];
+}
 
-export default function Work({projectsArchive} : IProps):JSX.Element{
-    return <ProjectsSection projectsArchive={projectsArchive} />
+export default function Work({
+  projectsArchive,
+}: IProps): JSX.Element {
+  return (
+    <>
+      <h1 id="archive">WORK</h1>
+      <p className="description">This is a showcase of the projects that I am most proud of. It is a mixture of front end full-stack projects. I am always learning to use new technologies.</p>
+      <ul id="archive_list">
+        {projectsArchive.map((project) => (
+          // <Link to={`/${project.id}`} key={project.id}>
+          <li
+            className="archive_project"
+            key={project.id}
+            // style={{ backgroundImage: `url(${project.image})` }}
+          >
+            <div id="archive_project_details">
+              <h2 className="archive_project_title">
+                {project.name.toLowerCase()}
+              </h2>
+              <p className="archive_project_description">
+                {project.description.length < 300
+                  ? project.description
+                  : `${project.description.slice(0, 297)}...`}
+              </p>
+              <div className="tags_div">
+                <ul className="tags">
+                  {project.tags.map((tag) => (
+                    <li key={project.tags.indexOf(tag)} className="tag">
+                      {tag}
+                    </li>
+                  ))}
+                </ul>
+
+                <div className="project_links">
+                  {project.front_end?.deployed_link !== "undefined" && (
+                    <img
+                      className="project_link"
+                      src="./img/box-arrow-up-right.svg"
+                      alt="link to deployed project"
+                      onClick={() =>
+                        window.open(project.front_end?.deployed_link, "_blank")
+                      }
+                    />
+                  )}
+                  {project.front_end?.github_link !== "undefined" && (
+                    <img
+                      className="project_link"
+                      src="./img/github.svg"
+                      alt="link to github repo"
+                      onClick={() =>
+                        window.open(project.front_end?.github_link, "_blank")
+                      }
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
+          </li>
+          // </Link>
+        ))}
+      </ul>
+    </>
+  );
 }
