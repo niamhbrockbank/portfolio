366561a37d5cd529f2b4b724e5d32a0fd13a57ea
niamhbrockbank <niamh.brockbank@gmail.com>
2022-11-13 20:31:08 +0000

    Add info to contact page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import projectsArchive from "./projectsArchive.json";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Work from "./components/Work/Work";
-import About from "./components/About";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 
 function App(): JSX.Element {
   return (
@@ -25,7 +26,7 @@ function App(): JSX.Element {
         <Route path="/about" element={<About />} />
         <Route
           path="/contact"
-          //element={<Contact />}
+          element={<Contact />}
         />
         <Route path="*" element={<Home />} />
       </Routes>
---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,14 @@
+export default function Contact():JSX.Element{
+    return (
+        <>
+        <h1>Contact</h1>
+        <p>I’m always happy to get a message! <br/><br/>
+
+
+Email : niamh.brockbank@gmail.com<br/>
+
+Find me on the internet: <a href="https://www.linkedin.com/in/niamh-brockbank/">LinkedIn</a> / <a href="https://github.com/niamhbrockbank">GitHub</a> / <a href="https://discordapp.com/users/872920095180075038">Discord</a>
+</p>
+        </>
+    )
+}
