37bd0291a9a75075977292dc9c7449bd286d38b7
niamhbrockbank <niamh.brockbank@gmail.com>
2022-11-14 13:14:47 +0000

    Improve page descriptions

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -13,13 +13,10 @@ export default function About(): JSX.Element {
         <div>
           <p className="page_description">
             Hello, my name is Niamh Brockbank. I am a recent graduate of the
-            Academy software development program. I am proficient in JavaScript,
+            <a href="http://www.academy.tech">Academy</a> software development program. I am proficient in JavaScript,
             and a big fan of TypeScript.
             <br />
-            <br />I have been numerous types of engineer and am most excited
-            about software engineer. I am passionate about the potential of new
-            technologies to solve the world’s biggest problems. I am a big
-            sustainability advocate and that influences everything I do.
+            <br />I have been numerous types of engineer and am motivated by solving real problems to improve people's lives. I am excited about the potential of new technologies to solve the world’s biggest problems, this drives my pursuit of continous personal and professional development. I am passionate about the value of diverse teams and multi-discplinary working I believe collaborative and holistic approaches to problems find the best, most elegant and most sustainable solutions.
           </p>
           <a href="./Brockbank_Niamh_CV_221104.pdf" target="_blank">
             <button>MY FULL CV</button>
