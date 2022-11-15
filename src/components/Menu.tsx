5fb8cb80ed28efaaafdff7dc86c9679dc08187b3
niamhbrockbank <niamh.brockbank@gmail.com>
2022-11-15 11:58:13 +0000

    Underline current page in menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { useMediaQuery } from "react-responsive";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Menu(): JSX.Element {
+  const page = useLocation().pathname.slice(1)
+  console.log(page)
+
   const [showMenu, setShowMenu] = useState(false);
   const isSmallScreen = useMediaQuery({ query: "(max-width: 900px)" });
   const menuOptions = ["home", "work", "about", "contact"];
@@ -11,7 +14,11 @@ export default function Menu(): JSX.Element {
     return (
       <li key={i}>
         <h2>
+          {page === option ? 
+          <p style={{borderBottom: '0.1rem solid #41423e'}}>{option}</p>
+          :
           <Link to={`/${option !== "home" ? option : ""}`}>{option}</Link>
+          }
         </h2>
       </li>
     );
@@ -26,6 +33,7 @@ export default function Menu(): JSX.Element {
               src="./img/menu-list.svg"
               alt="menu list button"
               id="menu_button"
+              //TODO: control show menu with CSS so can add transition
               onClick={() => setShowMenu(!showMenu)}
             />
           </div>
